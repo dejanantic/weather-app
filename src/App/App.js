8e18d5442743ee680d3bf697eeaa02dbb1f02715
Dejan Antic <dex.antic@gmail.com>
2021-02-04 16:00:49 +0100

    Remove Firebase reference from App.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import SingleCityDetails from "../SingleCityDetails/SingleCityDetails";
-import app from '../firebase'
 import './App.css'
 
 // Add lazy loading of components
 
 export default function App() {
-  console.log(app)
   return (
     <div className="container">
       <Router>
