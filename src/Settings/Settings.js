d9f66ed72710b132498f1be978b6486b92f97aa8
Dejan Antic <dex.antic@gmail.com>
2021-02-08 18:00:21 +0100

    Create first draft of Settings component

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -13,9 +13,21 @@ export default function Settings() {
         </div>
         <section className="settings__update-profile">
           <h2 className="settings__setting-title">update profile</h2>
+          {/* Placeholder */}
+          <div className="settings__placeholder">
+            <p className="settings__placeholder-text">
+              Placeholder for profile info
+            </p>
+          </div>
         </section>
         <section className="settings__manage-cities">
           <h2 className="settings__setting-title">manage cities</h2>
+          {/* Placeholder */}
+          <div className="settings__placeholder">
+            <p className="settings__placeholder-text">
+              Placeholder for cities
+            </p>
+          </div>
         </section>
         <section className="settings__delete-account">
           <h2 className="settings__setting-title">delete account</h2>
