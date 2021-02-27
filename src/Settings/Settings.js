0ab8ba4be3d6574a47a06c1fc00bc390b6c30a08
Dejan Antic <dex.antic@gmail.com>
2021-02-27 17:20:21 +0100

    Restructure HTML and style of Settings component

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -12,24 +12,25 @@ export default function Settings() {
         <div className="settings__title-container">
           <h1 className="settings__title">account settings</h1>
         </div>
-        <section className="settings__update-profile">
-          <h2 className="settings__setting-title">update profile</h2>
-          <div className="settings__section">
-            <UpdateProfile />
-          </div>
-          {/* Placeholder */}
-        </section>
-        <section className="settings__manage-cities">
-          <h2 className="settings__setting-title">manage cities</h2>
-          {/* Placeholder */}
-          <div className="settings__section">
-            <p className="settings__section-text">Placeholder for cities</p>
-          </div>
-        </section>
-        <section className="settings__delete-account">
-          <h2 className="settings__setting-title">delete account</h2>
-          <DeleteAccount />
-        </section>
+        <div className="settings__sections">
+          <section className="settings__update-profile">
+            <h2 className="settings__setting-title">update profile</h2>
+            <div className="settings__section">
+              <UpdateProfile />
+            </div>
+          </section>
+          <section className="settings__manage-cities">
+            <h2 className="settings__setting-title">manage cities</h2>
+            {/* Placeholder */}
+            <div className="settings__section">
+              <p className="settings__section-text">Placeholder for cities</p>
+            </div>
+          </section>
+          <section className="settings__delete-account">
+            <h2 className="settings__setting-title">delete account</h2>
+            <DeleteAccount />
+          </section>
+        </div>
       </main>
     </>
   );
