67fb9866761f4b62d353e477463558e254e741c2
Dejan Antic <dex.antic@gmail.com>
2021-03-03 19:15:28 +0100

    Lazy load components

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
 import PrivateRoute from '../PrivateRoute/PrivateRoute'
-import Home from "../Home/Home";
-import Signup from "../Signup/Signup";
-import Login from "../Login/Login"
-import SingleCityDetails from "../SingleCityDetails/SingleCityDetails";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import ForgotPassword from "../ForgotPassword/ForgotPassword";
-import Settings from "../Settings/Settings";
+import Loading from "../Loading/Loading";
 
-// Add lazy loading of components
+// Lazy load components
+const Home = React.lazy(() => import("../Home/Home"));
+const Settings = React.lazy(() => import("../Settings/Settings"));
+const SingleCityDetails = React.lazy(() => import("../SingleCityDetails/SingleCityDetails"));
+const Signup = React.lazy(() => import("../Signup/Signup"));
+const Login = React.lazy(() => import("../Login/Login"));
+const ForgotPassword = React.lazy(() => import("../ForgotPassword/ForgotPassword"));
 
 export default function App() {
   return (
     <div className="container">
       <Router>
         <AuthProvider>
-          <Switch>
-            <PrivateRoute exact path="/" component={Home} />
-            <PrivateRoute path="/settings" component={Settings} />
-            <Route path="/details" component={SingleCityDetails} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/forgot-password" component={ForgotPassword} />
-          </Switch>
+          <Suspense fallback={<Loading loadingMessage="Loading app" />}>
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+              <PrivateRoute path="/settings" component={Settings} />
+              <Route path="/details" component={SingleCityDetails} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+              <Route render={() => <h1>404 Page not found</h1>} />
+            </Switch>
+          </Suspense>
         </AuthProvider>
       </Router>
       <ToastContainer position="bottom-right" />
